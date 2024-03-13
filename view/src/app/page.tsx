48ed73119b5e634b20643183600483d8c4a900b8
poporu <aobaiwaki@gmail.com>
2024-03-13 23:08:00 +0900

    スタートページを作成

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/quiz");
+  return (
+    <main>
+      <div>
+        <Button
+        onClick={() => {
+            router.push("/quiz");
+        }}
+        >Click to start</Button>
+      </div>
+    </main>
+  )
 }
