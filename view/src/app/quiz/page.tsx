58415a22542e84934bb9cb5358fd63c08a9a3063
poporu <aobaiwaki@gmail.com>
2024-03-14 02:16:17 +0900

    Quiz画面の右側のレイアウトを仮作成し,色をぽかカラーに変更

--- view/src/app/quiz/page.tsx
+++ view/src/app/quiz/page.tsx
@@ -7,31 +7,46 @@ import { Card } from "@/components/ui/card";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-wrap justify-around items-center h-screen bg-base">
+      <div className="flex flex-wrap justify-around items-center h-screen bg-basecolor">
         <div className="flex flex-col justify-around items-center h-full w-1/2 p-4">
-          <div className="flex w-full items-center">
-            <Card className="flex flex-col justify-around text-8xl text-center w-full h-40">
-              <p>
-                齊藤京子
-              </p>
+          <div className="flex flex-col justify-around w-4/5 h-full items-center">
+            <Card className="flex flex-col justify-around m-3 text-6xl text-center w-full h-30 bg-primarycolor border-accentcolor border-4">
+              <div className="m-4">
+                <p>
+                  齊藤京子
+                </p>
+              </div>
+            </Card>
+            <Card className="flex m-3 border-accentcolor border-4">
+              <Image
+                src="/img/member/saitou.jpg"
+                width={300}
+                height={300}
+                alt="saitou"
+                className="flex object-center object-cover"
+                priority
+              />
+            </Card>
+            <Card className="text-2xl text-center w-full bg-primarycolor border-accentcolor border-4">
+              <div className="m-3">
+                <p>
+                  好きなラーメン：日高屋のチゲ味噌ラーメン
+                </p>
+              </div>
             </Card>
           </div>
-          <Card className="">
-            <Image
-              src="/img/member/saitou.jpg"
-              width={400}
-              height={400}
-              alt="saitou"
-              className="object-center object-cover"
-            />
-          </Card>
-          <Card className="flex flex-col justify-around text-3xl text-center w-full h-40">
-            <p>
-              好きなラーメン：日高屋のチゲ味噌ラーメン
-            </p>
-          </Card>
         </div>
         <div className="flex flex-col h-full w-1/2 p-4 justify-around items-center">
+          <div className="flex items-center h-full w-full text-center">
+            <div className="h-full w-full border-accentcolor border-4">
+              {/* TODO: Penlight Component */}
+              <Card>Penlight1</Card>
+            </div>
+            <div className="h-full w-full border-accentcolor border-4">
+              {/* TODO: Penlight Component */}
+              <Card>Penlight2</Card>
+            </div>
+          </div>
           <Button>Submit Answer</Button>
         </div>
       </div>
