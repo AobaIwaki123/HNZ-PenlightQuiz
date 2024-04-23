ad992dd9a8bb5085cf73be0759dc4d1d8415cf3d
poporu <aobaiwaki@gmail.com>
2024-04-24 00:46:46 +0900

    Little Update

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     initMemberTables();
     // await getNextQuiz();
     // router.push("/quiz");
-    // alert("Sorry, this feature is not available yet.");
+    alert("Sorry, this feature is not available yet.");
     // alert("Please wait for the next update.")
     // alert("Thank you for your understanding.")
     // alert("Have a nice day!")
