ad2716fdd03ecb7261bf7c2001e70d297316bb7a
poporu <aobaiwaki@gmail.com>
2024-04-24 01:14:37 +0900

    Update init DB

--- view/src/api/initDb.ts
+++ view/src/api/initDb.ts
@@ -11,28 +11,31 @@ import legacyPenlight from "./template/legacy_penlight.json";
 const prisma = new PrismaClient();
 
 export const initMemberTables = async () => {
+  console.log("Initializing Member Tables")
   // Initialize Member Table
-  await prisma.member.deleteMany();
+  // await prisma.member.deleteMany();
   await prisma.member.createMany({ data: memberJson.data });
 
   // Initialize MemberImage Table
-  await prisma.memberImage.deleteMany();
+  // await prisma.memberImage.deleteMany();
   await prisma.memberImage.createMany({ data: memberImageJson.data });
 
   // Initialize MemberInfo Table
-  await prisma.memberInfo.deleteMany();
+  // await prisma.memberInfo.deleteMany();
   await prisma.memberInfo.createMany({ data: memberInfoJson.data });
 
   // Initialize Color Table
-  await prisma.color.deleteMany();
+  // await prisma.color.deleteMany();
   await prisma.color.createMany({ data: colorJson.data });
 
   // FIXME: Delete below later
   // Initialize LegacyPenlight Table
-  await prisma.penlight.deleteMany();
+  // await prisma.penlight.deleteMany();
   await prisma.penlight.createMany({ data: legacyPenlight.data });
 
   // Initialize LegacyPenlightQuiz Table
-  await prisma.penlight_quiz.deleteMany();
+  // await prisma.penlight_quiz.deleteMany();
   await prisma.penlight_quiz.createMany({ data: legacyPenlightQuizJson.data });
+
+  console.log("Finish Initialize Member Tables")
 };
