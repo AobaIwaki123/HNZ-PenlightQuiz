935c41453d6413c856fe95ada1b3de33b96e2a7c
AobaIwaki <aobaiwaki@gmail.com>
2024-05-26 21:08:15 +0900

    Add Server Actions to Control SQL with Prisma

--- view/src/api/quiz.ts
+++ view/src/api/quiz.ts
@@ -1,10 +1,42 @@
 "use server";
 
 import { PrismaClient, Prisma } from "@prisma/client";
+import { quizMemberInfoType } from "@/zustand/memberStore";
 
 const prisma = new PrismaClient();
 
-export const getQuiz = async () =>{
+export const getQuiz = async () => {
+  // Get Random Member
+  const randomMember = await getRandomMember();
+  // Validate randomMember
+  if (!randomMember) return;
+  
+  // Get Member Name
+  const memberName = randomMember.name;
+
+  // Get Member Info
+  const memberInfo = await getMemberInfo(memberName);
+  // Validate memberInfo
+  if (!memberInfo) return;
+
+  // Get Member Image
+  const memberImage = await getMemberImage(memberName);
+  // Validate memberImage
+  if (!memberImage) return;
+
+  const quizMember = {
+    name: memberName,
+    nickname: randomMember.nickname,
+    penlightLeft: randomMember.penlightLeft,
+    penlightRight: randomMember.penlightRight,
+    memberInfo: memberInfo.info,
+    memberImage: memberImage.official,
+  };
+
+  return quizMember;
+}
+
+const getRandomMember = async () =>{
   // Get 1 Member Randomly
   const memberCount = await prisma.member.count();
 
@@ -16,5 +48,23 @@ export const getQuiz = async () =>{
     },
   });
 
-  console.log(member);
+  return member;
+}
+
+const getMemberInfo = (memberName: string) => {
+  const memberInfo = prisma.memberInfo.findFirst({
+    where: {
+      name: memberName,
+    },
+  });
+  return memberInfo;
+}
+
+const getMemberImage = (memberName: string) => {
+  const memberImage = prisma.memberImage.findFirst({
+    where: {
+      name: memberName,
+    },
+  });
+  return memberImage;
 }
