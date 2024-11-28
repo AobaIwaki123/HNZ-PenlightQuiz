157da93bbca5c3377ee656b2c1511c97c9185ee8
AobaIwaki <aobaiwaki@gmail.com>
2024-11-28 15:46:12 +0900

    正解ページのプロト版

--- view/src/app/quiz/answer/page.tsx
+++ view/src/app/quiz/answer/page.tsx
@@ -3,20 +3,15 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
-import { QUIZ_DATA } from "@/lib/const";
 import { useSearchParams } from "next/navigation";
 import { judgeColorMatch } from "@/lib/quiz/judge";
 import { Suspense } from "react";
 import { getQuiz } from "@/api/quiz";
 import { useQuizMemberStore } from "@/zustand/memberStore";
-import { getPenlightName } from "@/api/quiz";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  const [leftColorNameJn, setLeftColorNameJn] = useState(String);
-  const [rightColorNameJn, setRightColorNameJn] = useState(String);
   const setMemberInfo = useQuizMemberStore((state) => state.setQuizMemberInfo);
 
   const quizMember = useQuizMemberStore((state) => state);
@@ -57,90 +52,45 @@ export default function Home() {
 
   return (
     <main>
-      <div className="flex h-screen bg-basecolor" id="main">
-        <div className="flex-col w-1/2" id="left">
-          <div
-            className="flex h-1/4 justify-center items-center text-2xl sm:text-5xl lg:text-7xl bg-accentcolor "
-            id=""
-          >
-            <Card
-              className="text-basecolor 
-              border-4 border-basecolor
-              "
-              id="answer"
-            >
-              <div className="m-3 object-center object-cover">
-                <Suspense>{IsCorrectAnswer()}</Suspense>
+      <div
+        className="flex flex-col h-screen bg-basecolor text-2xl sm:text-5xl lg:text-7xl"
+        id="main"
+      >
+        <div className="flex h-2/3 bg-primarycolor">
+          <div className="flex flex-col w-1/2 bg-basecolor">
+            <div className="flex h-1/2 bg-secondarycolor">
+              <div className="flex flex-auto items-center justify-center">
+                <p>正解</p>
               </div>
-            </Card>
-          </div>
-          <div
-            className="flex h-1/4 justify-center items-center 
-          text-2xl sm:text-5xl lg:text-7xl
-          bg-basecolor "
-            id="memberName"
-          >
-            <Card
-              className=" text-accentcolor 
-              border-4 border-accentcolor
-              bg-transparent"
-              id=""
-            >
-              <div className="m-5">{nickname}</div>
-            </Card>
-          </div>
-          <div
-            className="flex flex-col h-1/2
-            text-xl sm:text-3xl lg:text-5xl
-            bg-accentcolor"
-            id="penlightColor"
-          >
-            <div className="flex justify-center items-center h-1/2 w-full">
-              <Card className="border-4 text-basecolor border-basecolor">
-                <div className="m-5">{colorNameLeft}</div>
-              </Card>
             </div>
-            <div className="flex justify-center items-center h-1/2 w-full">
-              <Card className="border-4 text-basecolor border-basecolor">
-                <div className="m-5">{colorNameRight}</div>
-              </Card>
+            <div className="flex flex-auto bg-accentcolor">
+              <div className="flex flex-auto items-center justify-center">
+                <p>あだ名</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col flex-auto bg-primarycolor">
+            <div className="flex h-1/2 bg-accentcolor">
+              <div className="flex flex-auto items-center justify-center">
+                <p>カラー1</p>
+              </div>
+            </div>
+            <div className="flex flex-auto bg-secondarycolor">
+              <div className="flex flex-auto items-center justify-center">
+                <p>カラー2</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex-auto flex-col" id="right">
-          <div className="h-3/4  bg-secondarycolor" id="blank"></div>
-          <div className="flex h-1/4  bg-primarycolor" id="">
-            <div
-              className="flex justify-center items-center w-1/2 bg-primarycolor"
-              id="blank"
-            >
-              <Button
-                className="h-1/2
-                text-lg sm:text-xl lg:text-3xl
-                bg-secondarycolor  text-primarycolor
-                border-4 border-secondarycolor
-                hover:bg-primarycolor hover:text-secondarycolor hover:border-secondarycolor"
-                onClick={moveToTop}
-                id="nextButton"
-              >
-                Top Page
-              </Button>
+        <div className="flex flex-auto bg-penlight_pastelblue">
+          <div className="flex w-1/2 bg-primarycolor">
+            <div className="flex flex-auto items-center justify-center">
+              <Button>Top Page</Button>
             </div>
-            <div
-              className="flex flex-auto justify-center items-center bg-basecolor"
-              id=""
-            >
-              <Button
-                className="h-1/2
-                text-lg sm:text-xl lg:text-3xl
-                bg-transparent 
-                border-4 border-accentcolor
-                hover:bg-accentcolor hover:text-basecolor hover:border-transparent"
-                onClick={moveToQuiz}
-                id="nextButton"
-              >
-                Next Quiz
-              </Button>
+          </div>
+          <div className="flex flex-auto bg-basecolor">
+            <div className="flex flex-auto items-center justify-center">
+              <Button>Next Quiz</Button>
             </div>
           </div>
         </div>
